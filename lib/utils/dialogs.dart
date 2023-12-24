893d051f003298860a3cd7007ce13d94e3f0525c
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-12-24 18:03:45 +0530

    snack bar added

---
+++ lib/utils/dialogs.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class Dialogs {
+  //?? for showing snack bar ->
+  static void showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: cardColor,
+        content: Text(
+          message,
+          style: const TextStyle(
+            fontFamily: 'poppins_bold',
+            fontSize: 15.0,
+            letterSpacing: 0.8,
+            color: Colors.white,
+          ),
+        ),
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
+  }
+
+  //?? for showing progress bar ->
+  static void showProgressBar(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
